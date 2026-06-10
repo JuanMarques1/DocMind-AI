9bf8313af5a2238da09c0193aa211da2e9bd7717
JuanMarques1 <juanoliveira549@gmail.com>
2026-06-09 22:43:04 -0300

    feat: AuthContext e provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 // Design system: tokens first, then components (override Tailwind utilities).
 import "./styles/tokens.css";
@@ -10,7 +11,9 @@ import "./styles/ui.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
